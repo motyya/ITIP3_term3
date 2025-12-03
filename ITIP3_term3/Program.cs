a1b856c0caa5b3f22229949e594f8e666da1ff69
motyya <motyya@DESKTOP-I4047PT>
2025-12-03 18:17:22 +0300

    test2

--- ITIP3_term3/Program.cs
+++ ITIP3_term3/Program.cs
@@ -11,5 +11,24 @@ namespace ITIP3_term3
         static void Main(string[] args)
         {
         }
+        //csharp
+        static int CorrectNumInput(int startNum, int endNum, string prompt = "")
+        {
+            int num;
+            while (true)
+            {
+                if (!string.IsNullOrEmpty(prompt))
+                    Console.Write(prompt);
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out num) &&
+                    num >= startNum &&
+                    num <= endNum)
+                {
+                    break;
+                }
+                Console.Write($"Invalid input! Please enter a number between {startNum} and {endNum}: ");
+            }
+            return num;
+        }
     }
 }
